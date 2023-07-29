5ee806e87e8ead95a742d2b44c75e54f2c9b2b6b
Anastasii <107581322+Nastya0107@users.noreply.github.com>
2023-07-29 13:45:12 +0300

    сделала записку

--- Assets/Scripts/SelectionByEyes.cs
+++ Assets/Scripts/SelectionByEyes.cs
@@ -4,12 +4,7 @@ using UnityEngine;
 
 public class SelectionByEyes : MonoBehaviour
 {
- 
-    void Start()
-    {
-        
-    }
-
+    [SerializeField] GameObject note;
   
     void Update()
     {
@@ -17,11 +12,13 @@ public class SelectionByEyes : MonoBehaviour
         Ray ray = new Ray(transform.position, transform.forward);
         Debug.DrawRay(transform.position, transform.forward * 10);
         Physics.Raycast(ray, out hit);
-        if(hit.transform.gameObject.CompareTag("Object"))
+        if(hit.transform.gameObject.CompareTag("Note"))
         {
+            note.SetActive(true);
             if (Input.GetKeyDown(KeyCode.E))
             {
                 Destroy(hit.transform.gameObject);
+                note.SetActive(false);
             }
         }
     }
